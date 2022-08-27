880c9154a707abc190283f419f722e5f0145627d
felipemarinhodev <felipmarinho@gmail.com>
2022-08-27 10:26:56 -0700

    a3.2 - using colors of theme

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,39 @@
-// import * as S from "./styles";
-
 import React from "react";
+import styled from "styled-components";
 
+const StyledButton = styled.button<ButtonProps>`
+	cursor: pointer;
+	border: 0;
+	padding: 8px 14px;
+	transition: 0.3s;
+
+	${({ theme, variant }) => {
+		console.log("theme", theme.colors[variant]);
+
+		return {
+			backgroundColor: theme.colors[variant].main,
+			color: theme.colors[variant].text,
+			":hover": {
+				backgroundColor: theme.colors[variant].light,
+			},
+			":focus": {
+				backgroundColor: theme.colors[variant].dark,
+			},
+		};
+	}}
+`;
 interface ButtonProps {
+	variant?: "primary" | "accent";
 	disabled?: boolean;
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
 	children: React.ReactNode;
 }
 
-export default function Button({
-	children,
-	disabled = false,
-	...props
-}: ButtonProps) {
-	return (
-		<button {...props} disabled={disabled}>
-			{children}
-		</button>
-	);
+export default function Button({ children, ...props }: ButtonProps) {
+	return <StyledButton {...props}>{children}</StyledButton>;
 }
+
+Button.defaultProps = {
+	disabled: false,
+	variant: "primary",
+};
