95bf9944d60519a75ef3f06e663ab07dbeae50ad
felipemarinhodev <felipmarinho@gmail.com>
2022-08-26 21:46:36 -0700

    a2.4 - create Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 // import * as S from "./styles";
 
+import Button from "../src/components/Button";
+
 export default function HomeScreen() {
-  return (
-    <div>
-      <h1>Home page!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Home page!</h1>
+			<Button>Simple Button</Button>
+		</div>
+	);
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+// import * as S from "./styles";
+
+export default function Button({ children }) {
+	return <button>{children}</button>;
+}
